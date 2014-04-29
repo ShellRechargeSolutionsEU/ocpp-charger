6ca697f218ee5ce5de4d016916a540aa7fd93c09
Reinier Lamers <r.lamers@thenewmotion.com>
2014-04-29 13:32:00 +0200

    remove accidentally-left OcppCharger.scala

--- src/main/scala/com/thenewmotion/chargenetwork/ocpp/charger/OcppCharger.scala
+++ src/main/scala/com/thenewmotion/chargenetwork/ocpp/charger/OcppCharger.scala
@@ -2,19 +2,31 @@ package com.thenewmotion.chargenetwork.ocpp.charger
 
 import java.net.URI
 import dispatch.Http
-import akka.actor.Props
+import akka.actor.{ActorRef, Props}
 import com.thenewmotion.ocpp.soap.CentralSystemClient
 import com.thenewmotion.ocpp.Version
+import com.thenewmotion.ocpp.messages.CentralSystem
 
-class OcppCharger(chargerId: String,
+trait OcppCharger {
+  def chargerActor: ActorRef
+}
+
+class OcppSoapCharger(chargerId: String,
                   numConnectors: Int,
                   ocppVersion: Version.Value,
-                  centralSystemURL: URI,
+                  centralSystemUri: URI,
                   server: ChargerServer,
-                  http: Http = new Http) {
+                  http: Http = new Http) extends OcppCharger {
 
-  val client = CentralSystemClient(chargerId, ocppVersion, centralSystemURL, http, Some(server.url))
+  val client = CentralSystemClient(chargerId, ocppVersion, centralSystemUri, http, Some(server.url))
   val chargerActor = system.actorOf(Props(new ChargerActor(BosService(chargerId, client), numConnectors)))
   server.actor ! ChargerServer.Register(chargerId, new ChargePointService(chargerId, chargerActor))
+}
+
+class OcppJsonCharger(chargerId: String,
+                       numConnectors: Int,
+                       centralSystemUri: URI) extends OcppCharger {
+  val client: CentralSystem = new JsonCentralSystemClient(chargerId, centralSystemUri)
+  val chargerActor = system.actorOf(Props(new ChargerActor(BosService(chargerId, client), numConnectors)))
 }
 
